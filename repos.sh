6bfd65744f1c6ecdcf47978c6b938b496203f5b8
Dominggoes Isakh <drjisakh@gmail.com>
2020-05-30 21:50:20 +0000

    Update for Lineage 14.1

--- repos.sh
+++ repos.sh
@@ -3,14 +3,6 @@ SOURCE_REPO_URL="https://www.github.com/lineageos"
 CUSTOM_REPO_URL="https://www.github.com/rinando/"
 
 PROJECTS=(
-'build/soong		https://github.com/lineageos/android_build_soong	lineage lineage-16.0	https://github.com/rinando/android_build_soon		github	lineage-16.0	rebase'
-'frameworks/base	https://github.com/lineageos/android_frameworks_base	lineage	lineage-16.0 	https://github.com/rinando/android_frameworks_base	github	lineage-16.0	rebase'
-'vendor/samsung	https://github.com/themuppets/proprietary_vendor_samsung	themuppets	lineage-16.0	https://github.com/rinando/proprietary_vendor_samsung	github	lineage-16.0	rebase'
-'hardware/samsung	https://github.com/lineageos/android_hardware_samsung	lineage	lineage-16.0 	https://github.com/rinando/android_hardware_samsung	github	lineage-16.0	rebase'
-'device/samsung/galaxys2-common	https://github.com/lineageos/android_device_samsung_galaxys2-common	lineage	lineage-17.0 	https://github.com/rinando/android_device_samsung_galaxys2-common	github	lineage-16.0	checkout'
-'device/samsung/i9100	https://github.com/lineageos/android_device_samsung_i9100	lineage	lineage-16.0 	https://github.com/rinando/android_device_samsung_i9100	github	lineage-16.0	checkout'
-'device/samsung/smdk4412-common	https://github.com/lineageos/android_device_samsung_smdk4412-common	lineage	cm-14.1	https://github.com/rinando/android_device_samsung_smdk4412-common	github	lineage-16.0	checkout'
-'device/samsung/i9300	https://github.com/lineageos/android_device_samsung_i9300	lineage	cm-14.1 	https://github.com/rinando/android_device_samsung_i9300	github	lineage-16.0	checkout'
-'kernel/samsung/smdk4412	https://github.com/lineageos/android_kernel_samsung_smdk4412	lineage	lineage-16.0 	https://github.com/rinando/android_kernel_samsung_smdk4412	github	lineage-16.0	checkout'
+'vendor/samsung	https://github.com/themuppets/proprietary_vendor_samsung	themuppets	cm-14.1	https://github.com/rinando/proprietary_vendor_samsung	github	cm-14.1	rebase'
 )
 
