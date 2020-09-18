d8391bb62ef67439575623e0d5dd43bfbe26b95c
Dominggoes Isakh <drjisakh@gmail.com>
2020-09-18 18:12:49 +0200

    Prepare for Android 11/R/Lineage 18.0

--- repos.sh
+++ repos.sh
@@ -3,16 +3,15 @@ SOURCE_REPO_URL="https://www.github.com/lineageos"
 CUSTOM_REPO_URL="https://www.github.com/rinando/"
 
 PROJECTS=(
-'system/libhidl        https://android.googlesource.com/platform/system/libhidl      aosp    refs/tags/android-10.0.0_r41         https://github.com/rinando/android_system_libhidl       rinando lineage-17.1    rebase'
-'frameworks/base	https://github.com/lineageos/android_frameworks_base	lineage	lineage-17.1 	https://github.com/rinando/android_frameworks_base	rinando	lineage-17.1	rebase'
-'vendor/samsung	https://github.com/themuppets/proprietary_vendor_samsung	themuppets	lineage-17.1	https://github.com/rinando/proprietary_vendor_samsung	rinando	lineage-17.1	rebase'
-'hardware/samsung	https://github.com/lineageos/android_hardware_samsung	lineage	lineage-17.1 	https://github.com/rinando/android_hardware_samsung	rinando	lineage-17.1	rebase'
-'device/samsung/galaxys2-common	https://github.com/lineageos/android_device_samsung_galaxys2-common	lineage	lineage-17.0 	https://github.com/rinando/android_device_samsung_galaxys2-common	rinando	lineage-17.1	checkout'
-'device/samsung/i9100	https://github.com/lineageos/android_device_samsung_i9100	lineage	lineage-17.0 	https://github.com/rinando/android_device_samsung_i9100	rinando	lineage-17.1	checkout'
-'device/samsung/smdk4412-common	https://github.com/lineageos/android_device_samsung_smdk4412-common	lineage	cm-14.1	https://github.com/rinando/android_device_samsung_smdk4412-common	rinando	lineage-17.1	checkout'
-'device/samsung/i9300	https://github.com/lineageos/android_device_samsung_i9300	lineage	cm-14.1 	https://github.com/rinando/android_device_samsung_i9300	rinando	lineage-17.1	checkout'
-'kernel/samsung/smdk4412	https://github.com/lineageos/android_kernel_samsung_smdk4412	lineage	lineage-17.0 	https://github.com/rinando/android_kernel_samsung_smdk4412	rinando	lineage-17.1	checkout'
-'bionic	https://github.com/lineageos/android_bionic	lineage	lineage-17.1 	https://github.com/rinando/android_bionic	rinando	lineage-17.1	rebase'
-'packages/apps/Snap	https://github.com/lineageos/android_packages_apps_snap	lineage	lineage-17.1 	https://github.com/rinando/android_packages_apps_snap	rinando	lineage-17.1	rebase'
+'bionic				https://github.com/lineageos/android_bionic				aosp    	refs/tags/android-11.0.0_r1	https://github.com/rinando/android_bionic				rinando	lineage-18.0	rebase'
+'system/libhidl			https://android.googlesource.com/platform/system/libhidl      	aosp    	refs/tags/android-11.0.0_r1	https://github.com/rinando/android_system_libhidl			rinando	lineage-18.0	rebase'
+'frameworks/base			https://github.com/lineageos/android_frameworks_base			lineage	lineage-18.0			https://github.com/rinando/android_frameworks_base			rinando	lineage-18.0	rebase'
+'vendor/samsung			https://github.com/themuppets/proprietary_vendor_samsung		themuppets	lineage-17.1			https://github.com/rinando/proprietary_vendor_samsung		rinando	lineage-18.0	rebase'
+'hardware/samsung			https://github.com/lineageos/android_hardware_samsung		lineage	lineage-18.0			https://github.com/rinando/android_hardware_samsung			rinando	lineage-18.0	rebase'
+'device/samsung/galaxys2-common	https://github.com/lineageos/android_device_samsung_galaxys2-common	lineage	lineage-17.0			https://github.com/rinando/android_device_samsung_galaxys2-common	rinando	lineage-18.0	checkout'
+'device/samsung/i9100			https://github.com/lineageos/android_device_samsung_i9100		lineage	lineage-17.0 			https://github.com/rinando/android_device_samsung_i9100		rinando	lineage-18.0	checkout'
+'device/samsung/smdk4412-common	https://github.com/lineageos/android_device_samsung_smdk4412-common	lineage	cm-14.1			https://github.com/rinando/android_device_samsung_smdk4412-common	rinando	lineage-18.0	checkout'
+'device/samsung/i9300			https://github.com/lineageos/android_device_samsung_i9300		lineage	cm-14.1			https://github.com/rinando/android_device_samsung_i9300		rinando	lineage-18.0	checkout'
+'kernel/samsung/smdk4412		https://github.com/lineageos/android_kernel_samsung_smdk4412		lineage	lineage-17.0			https://github.com/rinando/android_kernel_samsung_smdk4412		rinando	lineage-18.0	checkout'
 )
 
