0b3f68b114aa651dc2e3be29490ba7df5f04dc92
Dominggoes Isakh <drjisakh@gmail.com>
2022-11-24 08:09:02 +0100

    Update to Android 13.0.0_r13

--- repos.sh
+++ repos.sh
@@ -12,24 +12,23 @@ PROJECTS=(
 
 # Legacy Kernel build
 'prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9	https://github.com/rinando/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9	rinando	lineage-17.1			https://github.com/rinando/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9			rinando	lineage-20.0	rebase'
-)
 
 # Legacy ADB Function FS
 'packages/modules/adb		https://github.com/lineageos/android_packages_modules_adb	github	lineage-20.0	https://github.com/rinando/android_packages_modules_adb		rinando	lineage-20.0	rebase'
 
 # Disable Memfd features
-'art				https://android.googlesource.com/platform/art			aosp	refs/tags/android-13.0.0_r11	https://github.com/rinando/android_art				rinando	lineage-20.0	rebase'
-'external/perfetto			https://android.googlesource.com/platform/external/perfetto	aosp	refs/tags/android-13.0.0_r11	https://github.com/rinando/android_external_perfetto	rinando	lineage-20.0	rebase'
+'art				https://android.googlesource.com/platform/art			aosp	refs/tags/android-13.0.0_r13	https://github.com/rinando/android_art				rinando	lineage-20.0	rebase'
+'external/perfetto			https://android.googlesource.com/platform/external/perfetto	aosp	refs/tags/android-13.0.0_r13	https://github.com/rinando/android_external_perfetto	rinando	lineage-20.0	rebase'
 
 # Disable BPF features
-'system/bpf			https://android.googlesource.com/platform/system/bpf		aosp	refs/tags/android-13.0.0_r11	https://github.com/rinando/android_system_bpf			rinando	lineage-20.0	rebase'
-'system/netd			https://github.com/lineageos/android_system_netd		aosp	refs/tags/android-13.0.0_r11	https://github.com/rinando/android_system_netd			rinando	lineage-20.0	rebase'
-'frameworks/libs/net		https://android.googlesource.com/platform/frameworks/libs/net 		aosp	refs/tags/android-13.0.0_r11	https://github.com/rinando/android_frameworks_libs_net		rinando	lineage-20.0	rebase'
+'system/bpf			https://android.googlesource.com/platform/system/bpf		aosp	refs/tags/android-13.0.0_r13	https://github.com/rinando/android_system_bpf			rinando	lineage-20.0	rebase'
+'system/netd			https://github.com/lineageos/android_system_netd		aosp	refs/tags/android-13.0.0_r13	https://github.com/rinando/android_system_netd			rinando	lineage-20.0	rebase'
+'frameworks/libs/net		https://android.googlesource.com/platform/frameworks/libs/net 		aosp	refs/tags/android-13.0.0_r13	https://github.com/rinando/android_frameworks_libs_net		rinando	lineage-20.0	rebase'
 'packages/modules/Connectivity			https://github.com/LineageOS/android_packages_modules_Connectivity		github	lineage-20.0	https://github.com/rinando/android_packages_modules_Connectivity			rinando	lineage-20.0	rebase'
-'packages/modules/NetworkStack			https://android.googlesource.com/platform/packages/modules/NetworkStack		aosp	refs/tags/android-13.0.0_r11	https://github.com/rINanDO/android_packages_modules_NetworkStack			rinando	lineage-20.0	rebase'
+'packages/modules/NetworkStack			https://android.googlesource.com/platform/packages/modules/NetworkStack		aosp	refs/tags/android-13.0.0_r13	https://github.com/rINanDO/android_packages_modules_NetworkStack			rinando	lineage-20.0	rebase'
 
 # Binder threadpool shrink
-'system/libhwbinder                            https://android.googlesource.com/platform/system/libhwbinder                    aosp    refs/tags/android-13.0.0_r11    https://github.com/rinando/android_system_libhwbinder                   rinando lineage-20.0 rebase'
+'system/libhwbinder                            https://android.googlesource.com/platform/system/libhwbinder                    aosp    refs/tags/android-13.0.0_r13    https://github.com/rinando/android_system_libhwbinder                   rinando lineage-20.0 rebase'
 
 
 # Graphical performance fix
@@ -54,4 +53,5 @@ PROJECTS=(
 'hardware/lineage/interfaces	https://github.com/LineageOS/android_hardware_lineage_interfaces	github	lineage-20.0	https://github.com/rinando/android_hardware_lineage_interfaces	rinando	lineage-20.0	rebase'
 
 # Fix bluetooth audio
-'packages/modules/Bluetooth                            https://android.googlesource.com/platform/packages/modules/Bluetooth                    aosp    refs/tags/android-13.0.0_r11    https://github.com/rinando/android_packages_modules_bluetooth                   rinando lineage-20.0 rebase'
+'packages/modules/Bluetooth                            https://android.googlesource.com/platform/packages/modules/Bluetooth                    github	lineage-20.0    https://github.com/rinando/android_packages_modules_bluetooth                   rinando lineage-20.0 rebase'
+)
